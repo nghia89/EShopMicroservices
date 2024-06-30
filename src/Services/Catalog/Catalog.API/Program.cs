80b39c3eecbf0736a7f88e31b87883a7af82d693
Nguyen Thanh Nghia <hangnghia11089@gmail.com>
2024-06-30 17:42:25 +0700

    add cqrs create product

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+ 
+app.MapCarter();
 
 app.Run();
