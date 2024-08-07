2b237811ab6132b32d52f99ae878fcb48cb6b6a6
Nguyen Thanh Nghia <hangnghia11089@gmail.com>
2024-08-07 22:43:55 +0700

    add migration db

---
+++ src/Services/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,19 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupons { get; set; } = default!;
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "Iphone x", Description = "This is phone x", Amount = 100 });
+        }
+    }
+}
