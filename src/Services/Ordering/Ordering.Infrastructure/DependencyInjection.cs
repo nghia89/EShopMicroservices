ed3a0a0efee684f6a7432a2bb5c16b3493715cd6
Nguyen Thanh Nghia <hangnghia11089@gmail.com>
2024-08-11 15:50:57 +0700

    add init Ordering

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+      (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            //// Add services to the container.
+            //services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            //services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            //services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            //{
+            //    options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            //    options.UseSqlServer(connectionString);
+            //});
+
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
