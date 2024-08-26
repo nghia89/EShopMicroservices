db80bae0f5526c64045339317835cfb5147d2645
Nguyen Thanh Nghia <hangnghia11089@gmail.com>
2024-08-26 22:17:01 +0700

    add minimal api ordering

--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -10,26 +10,13 @@ builder.Services
     .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices(builder.Configuration);
-
-builder.Services.AddControllers();
+ 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    await app.InitialiseDatabaseAsync();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseApiServices();
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
 app.Run();
