85ddb33f05b86e16fb639ec2ec215b574a90a2a7
nguyennghia <hangnghia11089@gmail.com>
2024-09-02 15:58:55 +0700

    up

--- src/Services/Basket/Basket.API/Basket/CheckOutBasket/CheckOutBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/CheckOutBasket/CheckOutBasketEndpoints.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.API.Basket.CheckOutBasket
+﻿using Basket.API.Dtos;
+namespace Basket.API.Basket.CheckOutBasket
 {
-    public class CheckOutBasketEndpoints
+    public record CheckOutBasketRequest(BasketCheckoutDto BasketCheckOutDto);
+    public record CheckoutBasketResponse(bool IsSuccess);
+    public class CheckOutBasketEndpoints : ICarterModule
     {
+        public AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket/checkout", async (CheckOutBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CheckoutBasketCommand>();
+                var result = await await sender.Send(command);
+
+                var response = result.Adapt<CheckoutBasketResponse>();
+
+                return result.Ok(response);
+            }).WithName("CheckoutBasket")
+            .Produces<CheckoutBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("CheckoutBasket"); ;
+        }
     }
 }
